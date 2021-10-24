27510fff8c9ea5e66b0d091a9e7bce255f0e1329
Mushinako <8977737+Mushinako@users.noreply.github.com>
2021-10-24 09:38:08 -0700

    Fig 2

--- mlp/optimisers.py
+++ mlp/optimisers.py
@@ -17,8 +17,16 @@ logger = logging.getLogger(__name__)
 class Optimiser(object):
     """Basic model optimiser."""
 
-    def __init__(self, model, error, learning_rule, train_dataset,
-                 valid_dataset=None, data_monitors=None, notebook=False):
+    def __init__(
+        self,
+        model,
+        error,
+        learning_rule,
+        train_dataset,
+        valid_dataset=None,
+        data_monitors=None,
+        notebook=False,
+    ):
         """Create a new optimiser instance.
 
         Args:
@@ -40,7 +48,7 @@ class Optimiser(object):
         self.learning_rule.initialise(self.model.params)
         self.train_dataset = train_dataset
         self.valid_dataset = valid_dataset
-        self.data_monitors = OrderedDict([('error', error)])
+        self.data_monitors = OrderedDict([("error", error)])
         if data_monitors is not None:
             self.data_monitors.update(data_monitors)
         self.notebook = notebook
@@ -57,15 +65,22 @@ class Optimiser(object):
         respect to all the model parameters and then updates the model
         parameters according to the learning rule.
         """
-        with self.tqdm_progress(total=self.train_dataset.num_batches) as train_progress_bar:
-            train_progress_bar.set_description("Ep Prog")
-            for inputs_batch, targets_batch in self.train_dataset:
-                activations = self.model.fprop(inputs_batch)
-                grads_wrt_outputs = self.error.grad(activations[-1], targets_batch)
-                grads_wrt_params = self.model.grads_wrt_params(
-                    activations, grads_wrt_outputs)
-                self.learning_rule.update_params(grads_wrt_params)
-                train_progress_bar.update(1)
+        # with self.tqdm_progress(total=self.train_dataset.num_batches) as train_progress_bar:
+        #     train_progress_bar.set_description("Ep Prog")
+        #     for inputs_batch, targets_batch in self.train_dataset:
+        #         activations = self.model.fprop(inputs_batch)
+        #         grads_wrt_outputs = self.error.grad(activations[-1], targets_batch)
+        #         grads_wrt_params = self.model.grads_wrt_params(
+        #             activations, grads_wrt_outputs)
+        #         self.learning_rule.update_params(grads_wrt_params)
+        #         train_progress_bar.update(1)
+        for inputs_batch, targets_batch in self.train_dataset:
+            activations = self.model.fprop(inputs_batch)
+            grads_wrt_outputs = self.error.grad(activations[-1], targets_batch)
+            grads_wrt_params = self.model.grads_wrt_params(
+                activations, grads_wrt_outputs
+            )
+            self.learning_rule.update_params(grads_wrt_params)
 
     def eval_monitors(self, dataset, label):
         """Evaluates the monitors for the given dataset.
@@ -77,13 +92,15 @@ class Optimiser(object):
         Returns:
             OrderedDict of monitor values evaluated on dataset.
         """
-        data_mon_vals = OrderedDict([(key + label, 0.) for key
-                                     in self.data_monitors.keys()])
+        data_mon_vals = OrderedDict(
+            [(key + label, 0.0) for key in self.data_monitors.keys()]
+        )
         for inputs_batch, targets_batch in dataset:
             activations = self.model.fprop(inputs_batch, evaluation=True)
             for key, data_monitor in self.data_monitors.items():
                 data_mon_vals[key + label] += data_monitor(
-                    activations[-1], targets_batch)
+                    activations[-1], targets_batch
+                )
         for key, data_monitor in self.data_monitors.items():
             data_mon_vals[key + label] /= dataset.num_batches
         return data_mon_vals
@@ -96,10 +113,9 @@ class Optimiser(object):
             values corresponding to the value of the statistic.
         """
         epoch_stats = OrderedDict()
-        epoch_stats.update(self.eval_monitors(self.train_dataset, '(train)'))
+        epoch_stats.update(self.eval_monitors(self.train_dataset, "(train)"))
         if self.valid_dataset is not None:
-            epoch_stats.update(self.eval_monitors(
-                self.valid_dataset, '(valid)'))
+            epoch_stats.update(self.eval_monitors(self.valid_dataset, "(valid)"))
         return epoch_stats
 
     def log_stats(self, epoch, epoch_time, stats):
@@ -110,10 +126,13 @@ class Optimiser(object):
             epoch_time: Time taken in seconds for the epoch to complete.
             stats: Monitored stats for the epoch.
         """
-        logger.info('Epoch {0}: {1:.1f}s to complete\n    {2}'.format(
-            epoch, epoch_time,
-            ', '.join(['{}={:.2e}'.format(k, v) for (k, v) in stats.items()])
-        ))
+        logger.info(
+            "Epoch {0}: {1:.1f}s to complete\n    {2}".format(
+                epoch,
+                epoch_time,
+                ", ".join(["{}={:.2e}".format(k, v) for (k, v) in stats.items()]),
+            )
+        )
 
     def train(self, num_epochs, stats_interval=5):
         """Trains a model for a set number of epochs.
@@ -136,7 +155,7 @@ class Optimiser(object):
             for epoch in range(1, num_epochs + 1):
                 start_time = time.time()
                 self.do_training_epoch()
-                epoch_time = time.time()- start_time
+                epoch_time = time.time() - start_time
                 if epoch % stats_interval == 0:
                     stats = self.get_epoch_stats()
                     self.log_stats(epoch, epoch_time, stats)
@@ -144,5 +163,8 @@ class Optimiser(object):
                 progress_bar.update(1)
         finish_train_time = time.time()
         total_train_time = finish_train_time - start_train_time
-        return np.array(run_stats), {k: i for i, k in enumerate(stats.keys())}, total_train_time
-
+        return (
+            np.array(run_stats),
+            {k: i for i, k in enumerate(stats.keys())},
+            total_train_time,
+        )
